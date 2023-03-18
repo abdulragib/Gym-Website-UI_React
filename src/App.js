012e69dd6e6e9fb5d52b519c841cfceb71f90a6c
Abdul Ragib <abdulragib36@gmail.com>
2023-03-18 18:35:16 +0530

    Reason and Program section done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Hero from './components/Hero/Hero.jsx';
+import Programs from './components/Programs/Programs';
+import Reasons from './components/Reasons/Reasons';
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+          <Hero/>
+          <Programs/>
+          <Reasons/>
     </div>
   );
 }
