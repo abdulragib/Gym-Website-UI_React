c1738b9c2bd63093497dffd159987fc710b31ddc
Abdul Ragib <abdulragib36@gmail.com>
2023-03-19 14:55:19 +0530

    Plans Card done

--- src/components/Programs/Programs.jsx
+++ src/components/Programs/Programs.jsx
@@ -13,8 +13,8 @@ const Programs = () => {
         </div>
 
         <div className='program-categories'>
-            {programsData.map(program => (
-                <div className='category' key={program.id}>
+            {programsData.map((program,index) => (
+                <div className='category' key={index}>
                    {program.image}
                    <span>{program.heading}</span>
                    <span>{program.details}</span>
